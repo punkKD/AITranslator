ea425c78b9f9f2d6aafcfea636eda0bfabe3f070
punkKD <pungkahding@gmail.com>
2026-09-14 14:45:02 +0800

    Update: Looped & formatting changed

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from translator import *
 
-source_text = input("Enter text to translate: ")
-source_lang, confidence = detect_language(source_text)
-target_lang = "English"
-print(f"Detected Language: {source_lang} (Confidence: {confidence})\n\n")
-translated_text = translate_adaptive(source_text, source_lang, target_lang) 
-print(f"Translated Text: {translated_text}")   
+while True:
+    source_text = input("Enter text to translate: ")
+    source_lang, confidence = detect_language(source_text)
+    target_lang = "English"
+    print(f"Detected Language: {source_lang} (Confidence: {confidence})\n\n")
+    translated_text = translate_adaptive(source_text, source_lang, target_lang) 
+    print(f"Translated Text: \n{translated_text}\n\n")   
