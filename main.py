a23088ae8501b42f7bf398f58e5241277418a5dc
punkKD <pungkahding@gmail.com>
2026-09-14 13:52:10 +0800

    Update: Shifted print statement

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from translator import *
 source_text = input("Enter text to translate: ")
 source_lang, confidence = detect_language(source_text)
 target_lang = "English"
+print(f"Detected Language: {source_lang} (Confidence: {confidence})\n\n")
 translated_text = translate_adaptive(source_text, source_lang, target_lang) 
-print(f"Detected Language: {source_lang} (Confidence: {confidence})")
 print(f"Translated Text: {translated_text}")   
